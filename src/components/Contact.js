f6eeb6beebe1ee852a1de31ba3ad61009366510a
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2024-02-16 15:54:01 -0500

    pages fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Nav from './nav';
+import Nav from './Nav';
 import Home from './Home';
 import Resume from './Resume';
 
