7f061b9275925105190a8dd5415180e9058659dd
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2024-02-23 15:50:32 -0500

    update

---
+++ src/components/Data.js
@@ -0,0 +1,21 @@
+const NYU = {
+    name: 'New York University',
+    graduationDate: 'MAY 2024',
+    major: 'B.S. Computer Science',
+    minors: ['Mathematics', 'Integrated Design Media'],
+    gpa: '3.2',
+    relevantCoursework: [
+      'Programming & Problem Solving',
+      'Data Structures & Algorithms',
+      'Object Oriented Programming',
+      'Computer Architecture & Organization',
+      'Design and Analysis of Algorithms',
+      'Dynamic Web',
+      'Human Computer Interaction',
+      'Databases',
+      'Game Programming',
+    ],
+  };
+  
+  export { NYU };
+  
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import '../App.css';
+import Dropdown from './Dropdown'; // Import the Dropdown component
+import { NYU } from './Data'; // Adjust the path as needed
 
-const TextBox = ({ value, onChange }) => {
+const TextBox = ({ text }) => {
+  let renderedText;
+
+  // Determine which text to render based on the prop value
+  if (text === 'homeText') {
+    renderedText = 'This is home text.';
+  } else if (text === 'resumeText') {
+    renderedText = <Dropdown title="EDUCATION" data={NYU} />;
+  } else if (text === 'contactText') {
+    renderedText = 'This is contact text.';
+  } else {
+    renderedText = 'No text specified.';
+  }
+
   return (
-    <div
-      className="textBox" 
-    />
+    <div className="textBox">
+      <div className="textBox-content">{renderedText}</div>
+    </div>
   );
-}
+};
 
 export default TextBox;
