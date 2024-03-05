5dcde1c5b44f9070f13b538bd0921065fc2de5ab
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2024-03-05 15:53:12 -0500

    styling update

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,21 +4,23 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Dropdown = ({ title, data }) => {
   return (
-    <Accordion>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography>{title}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <div>
-          <p>Name: {data.name}</p>
-          <p>Graduation Date: {data.graduationDate}</p>
-          <p>Major: {data.major}</p>
-          <p>Minors: {data.minors.join(', ')}</p>
-          <p>GPA: {data.gpa}</p>
-          <p>Relevant Coursework: {data.relevantCoursework.join(', ')}</p>
-        </div>
-      </AccordionDetails>
-    </Accordion>
+    <div className="dropdown-container">
+      <Accordion style={{ maxWidth: '900px' }}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          {title}
+        </AccordionSummary>
+        <AccordionDetails>
+          <div>
+            <p>Name: {data.name}</p>
+            <p>Graduation Date: {data.graduationDate}</p>
+            <p>Major: {data.major}</p>
+            <p>Minors: {data.minors.join(', ')}</p>
+            <p>GPA: {data.gpa}</p>
+            <p>Relevant Coursework: {data.relevantCoursework.join(', ')}</p>
+          </div>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   );
 };
 
