953d2defa21632418d99bd29c0b8546c61bedd49
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2024-12-02 15:12:53 -0500

    new bio

---
+++ src/components/Data.js
@@ -0,0 +1,73 @@
+const NYU = {
+    name: 'New York University',
+    graduationDate: 'MAY 2024',
+    major: 'B.S. Computer Science',
+    minors: ['Mathematics', 'Integrated Design Media'],
+    achievements: [
+      'NYU Tandon Scholarship Recipient',
+      'INROADS Internship Program Participant',
+      'Vice President of Alpha Phi Omega Beta Iota, a Community Service organization',
+    ],
+  };
+
+const L3Harris = {
+  name: 'L3Harris',
+  title: 'Software Engineer',
+  workDates: 'MAY 2023 - AUG 2023',
+  location: 'San Diego',
+  overview: [
+    'Developed a range of reusable React components to enhance user interface consistency',
+    'Actively participated in Agile sprints, collaborating with cross-functional team members to ensure seamless integration of React components.',
+    'Gained substantial expertise in front-end programming and design principles through hands-on experience with React and Material UI and strengthened problem-solving abilities by addressing challenges related to UI and UX.',
+    'Utilized XAMPP to set up a local development environment and efficiently managed MySQL databases using phpMyAdmin, performing SQL database manipulations such as creating, modifying, and querying tables to support web application development.',
+  ],
+ };
+
+ const Cooper = {
+  name: 'Cooper Union',
+  title: 'Programming Instructor',
+  workDates: 'JUNE 2021 - AUG 2022',
+  location: 'New York',
+  overview: [
+    'Implemented Python, JavaScript and ReactJS to create a web application for scheduling college and life goals.',
+    'Collaborated with a start-up company and high school students using agile development skills to design a web application to their liking while also using feature lists and wireframes to organize work into a shorter time period.',
+    'Managed the front and back-end design of the application in a team to create a working demo within six weeks.',
+  ],
+ };
+
+ const Subway = {
+  name: 'Subway Route Planner Webapp',
+  overview: [
+    'Achieved successful connections for both local and cloud MongoDB databases, and implemented backup and restore scripts for database collections.',
+    'Created and populated key databases to store user information, routes, train schedules, and bus data.',
+    'Developed API models to handle user data, routes, and transportation information.',
+  ],
+ };
+
+ const Games = {
+  name: 'C++ & OpenGL Games',
+  overview: [
+    'Designed and implemented custom graphics and shaders, handling all aspects of visual rendering.',
+    'Utilized OpenGL to manage 2D rendering, texture mapping, and real-time graphics in game development.',
+    'Applied advanced OOP principles, including encapsulation, inheritance, and polymorphism, in game design and development.',
+  ],
+ };
+
+ const Contact = {
+  phoneNumber: '929-278-9708',
+  email: 'amy.fouzia00@gmail.com'
+ }
+
+ const AboutMe = {
+  About: `I am a highly motivated and detail-oriented engineer that demonstrates a strong proficiency in multiple programming languages including Python, C++, JavaScript, and SQL.
+  I have practical experience in front-end development using React and Material UI as well as database manipulation. 
+  I have a proven track record of effective collaboration in Agile environments, excellent problem-solving skills, and the ability to deliver high-quality software solutions. 
+  I'm also involved in community service and leadership as Vice President of Alpha Phi Omega Beta Iota. 
+  I like to continuously expand my technical skills through personal projects, such as this website!
+  
+  I recently graduated from NYU with a bachelors degree Computer Science, as well as minors in Mathematics and Deigital Design.
+  I am now searching for a job opportunity with a company where I can expand my skills and learn new things.`,
+};
+
+export { NYU, L3Harris, Cooper, Subway, Games, Contact, AboutMe };
+  
