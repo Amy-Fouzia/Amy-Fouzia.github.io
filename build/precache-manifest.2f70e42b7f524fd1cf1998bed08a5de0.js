ac885462a8a967de85640136ff1940a10d5bc4c4
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2024-12-11 18:31:40 -0500

    projects

---
+++ build/precache-manifest.2f70e42b7f524fd1cf1998bed08a5de0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd95b7212d57f232f032c9b27d2b0b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64128157d3811ea4f952",
+    "url": "/static/css/main.44aabcfb.chunk.css"
+  },
+  {
+    "revision": "e884863c0a8c7146d73d",
+    "url": "/static/js/2.4eeb0394.chunk.js"
+  },
+  {
+    "revision": "e3a5989edcaf146607c5",
+    "url": "/static/js/3.8f08ea35.chunk.js"
+  },
+  {
+    "revision": "64128157d3811ea4f952",
+    "url": "/static/js/main.5fd371c9.chunk.js"
+  },
+  {
+    "revision": "d1c315b404d5559ea9d1",
+    "url": "/static/js/runtime~main.75876787.js"
+  },
+  {
+    "revision": "6ac2901e1994a46655e815e962b36ea7",
+    "url": "/static/media/EconomicConditions.6ac2901e.ttf"
+  },
+  {
+    "revision": "8adaf6fe132b7840bc18acb451b49e37",
+    "url": "/static/media/background.8adaf6fe.png"
+  },
+  {
+    "revision": "8d31f6ebdfc3f18c553ec6d6e93cf290",
+    "url": "/static/media/blue_fish.8d31f6eb.png"
+  },
+  {
+    "revision": "49b5ed7a34ed0ab7b54b1094d9669a14",
+    "url": "/static/media/me.49b5ed7a.jpg"
+  },
+  {
+    "revision": "9b20fad7afe44464125611d3bcf7f367",
+    "url": "/static/media/myName.9b20fad7.m4a"
+  },
+  {
+    "revision": "be688ed129ccbe7563250f0d66c9ab28",
+    "url": "/static/media/pink_fish.be688ed1.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e527b95319d41366421ab82a56452c9c.js"
+  "/precache-manifest.2f70e42b7f524fd1cf1998bed08a5de0.js"
 );
 
 self.addEventListener('message', (event) => {
